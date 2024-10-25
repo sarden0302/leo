5e2d6d17ead43c835ffdef72a0afdef9aec9cb82
sarden0302 <59376161+sarden0302@users.noreply.github.com>
2024-10-25 16:07:15 +0900

    Delete cloth.js

--- cloth.js
+++
@@ -1,7 +0,0 @@
-$('.menu li').mouseenter(function(){
-    $(this).children('.sub-menu').stop().slideDown()
-})
-
-$('.menu li').mouseleave(function(){
-    $(this).children('.sub-menu').stop().slideUp()
-})
